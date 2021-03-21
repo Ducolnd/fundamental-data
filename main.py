094ffef35818a46b3198cde396f381fb9fdb3a04
Duco Lindhout <duco.lindhout@gmail.com>
2021-03-21 18:59:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from yahoofinancials import YahooFinancials
+import pandas as pd
+
+
+# Prepare yahoo finance call
+tickers = ["TSLA"]
+
+data = YahooFinancials(tickers)
+
+income = data.get_financial_stmts('annualy', 'income')
+balance = data.get_financial_stmts('annualy', 'balance')
+cash = data.get_financial_stmts('annualy', 'cash')
+
+
+df = pd.DataFrame()
+
+def put_in(thing, what):
+    temp = pd.DataFrame()
+
+    for (ticker, value) in list(what.values())[0].items():
+        for x in value:
+            for (date, data) in x.items():
+                print(date)
+                temp = temp.append(pd.Series(data, name=ticker))
+
+                # We only want most recent data so the rest we don't care about
+                break
+            break
+    
+    # Rotate
+    temp = temp.T
+
+    # For multiindex
+    temp.index.name = "data"
+    temp["type"] = list(what.keys())[0]
+
+    return pd.concat([thing, temp], axis=0)
+    
+    
+# df = put_in(df, balance)
+df = put_in(df, cash)
+df = put_in(df, income)
+
+# Fix multiindex
+df = df.reset_index()
+df = df.set_index(["type", "data"])
+
+# Write
+df.to_csv("out.csv", index=True)
